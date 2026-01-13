44113309e45e2ac1bb55c6a5c7ae02401ca99e77
mhwagoner <mhwagone@ucsc.edu>
2026-01-12 18:56:27 -0800

    Add files via upload

---
+++ MW-Making-a-Scene/src/main.js
@@ -0,0 +1,12 @@
+// Code Practice: Making a Scene
+// Name: Michael Wagoner
+// Date: 1/12/26
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [ MainMenu, Play ]
+}
+
+let game = new Phaser.Game(config)
