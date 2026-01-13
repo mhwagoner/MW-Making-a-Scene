1bc1c2f8d46ffa1d15e2bb3445209089961fc54e
mhwagoner <mhwagone@ucsc.edu>
2026-01-12 18:58:24 -0800

    Add files via upload

---
+++ src/scenes/MainMenu.js
@@ -0,0 +1,26 @@
+class MainMenu extends Phaser.Scene {
+    constructor() {
+        super('mainMenuScene');
+        console.log('MainMenu: constructor');
+    }
+
+    init() {
+        console.log('MainMenu: init');
+        this.HP = 100;
+        this.EXP = 0;
+        console.log(`HP: ${this.HP} EXP: ${this.EXP}`);
+    }
+
+    create() {
+        console.log('MainMenu: create');
+
+        let playerStats = {
+            HP: this.HP,
+            EXP: this.EXP
+        };
+
+        console.log(playerStats.HP);
+
+        this.scene.start('playScene', playerStats);
+    }
+}
